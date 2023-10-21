249d58d592ddd091560fa54e0b4444e57029aed5
Denílson_S_Araújo <91626655+denysaraujo@users.noreply.github.com>
2023-10-21 09:30:17 -0300

    Update plugins.php

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -2,9 +2,13 @@
 
 return [
     'plugins' => [
-        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],        
+        'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        //'Accessibility' => ['namespace' => 'Accessibility'],
         'MultipleLocalAuth' => ['namespace' => 'MultipleLocalAuth'],
+        'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
+        //'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
+        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],
         'SendEmailUser' => ['namespace' => 'SendEmailUser'],
-        'Report' => ['namespace' => 'Report'],                
+        'Report' => ['namespace' => 'Report'],              
     ]
 ];
