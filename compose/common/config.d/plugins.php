30155182aed044bf6872722acc57afe39055673c
Denílson_S_Araújo <91626655+denysaraujo@users.noreply.github.com>
2023-11-20 23:22:03 -0300

    Include ChatTawkto

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -9,7 +9,6 @@ return [
         //'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],
         'SendEmailUser' => ['namespace' => 'SendEmailUser'],
-        'Report' => ['namespace' => 'Report'],
-        'ChatTawkto' =>['namespace' => 'ChatTawkto', 'config' => ['chat_link_url' => 'https://tawk.to/chat/655c005191e5c13bb5b2063b/1hfnm2g4p',],],              
+        'Report' => ['namespace' => 'Report'],                       
     ]
 ];
--- themes/SantaLuziaMG/layouts/parts/footer.php
+++ themes/SantaLuziaMG/layouts/parts/footer.php
@@ -27,6 +27,18 @@
 <script>
 new window.VLibras.Widget('https://vlibras.gov.br/app');
 </script>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/655c005191e5c13bb5b2063b/1hfnm2g4p';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
