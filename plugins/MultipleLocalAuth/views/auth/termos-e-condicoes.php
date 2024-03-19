3caf9a1551151dab686764bc43d77bd4124c2ad7
Denílson_S_Araújo <91626655+denysaraujo@users.noreply.github.com>
2024-03-19 09:41:42 -0300

    Atualização dos termos de uso.

--- plugins/MultipleLocalAuth/views/auth/termos-e-condicoes.php
+++ plugins/MultipleLocalAuth/views/auth/termos-e-condicoes.php
@@ -3,20 +3,20 @@
 <div style="padding: 50px; margin: 50px;">
 
 
-<h1>Termo de Uso</h1>
-<p>
+<h1 style="text-align:justify">Termo de Uso</h1>
+<p style="text-align:justify">
 Atenção: Leia atentamente os termos abaixo antes de utilizar este site.
 O Mapa Cultural  é um serviço interativo disponibilizado por meio de página eletrônica na internet que oferece informações sobre cultura, agentes cadastrados, serviços, chamadas, editais, espaços culturais, sociais e públicos etc. O acesso ao Mapa representa a aceitação expressa e irrestrita dos termos de uso abaixo descritos. Se você não concorda com os termos não utilize este site.
 
-<h1>I. Introdução</h1>
-<p>
+<h1 style="text-align:justify">I. Introdução</h1>
+<p style="text-align:justify">
 Este Termo de Uso descreve as condições de uso do serviço Mapa Cultural , incluindo os direitos e deveres de seus titulares e a política de uso de seus dados pessoais.
 O Mapa Cultural  é uma plataforma livre, gratuita e colaborativa de mapeamento da Secretaria da Cultura do Estado  sobre o cenário cultural .
 O serviço Mapa Cultural  é gratuito e sua utilização é facultada ao cidadã(ão) que, ao utilizá-lo, adere aos termos aqui dispostos, concordando e se submetendo integralmente às suas condições.
 </p>
 
-<h1>II. Dos Serviços</h1>
-<p>
+<h1 style="text-align:justify">II. Dos Serviços</h1>
+<p style="text-align:justify">
 Os serviços ofertados pela plataforma são os seguintes:
 Cadastramento de espaços, agentes, eventos, projetos e oportunidades;
 Consulta aos cadastros de espaços, de agentes, de eventos, de projetos e de oportunidades;
@@ -26,8 +26,8 @@ Execução, acompanhamento e publicação dos resultados de projetos e oportunid
 Novos serviços poderão ser disponibilizados a qualquer momento e os serviços existentes poderão ser removidos mediante comunicação através do site <a href="https://mapacultural.santaluzia.mg.gov.br">https://mapacultural.santaluzia.mg.gov.br</a> 
 </p>
 
-<h1>III. Da Qualificação das Partes</h1>
-<p>
+<h1 style="text-align:justify">III. Da Qualificação das Partes</h1>
+<p style="text-align:justify">
 No escopo da Mapa Cultural , as seguintes qualificações se aplicam:
 Titular: pessoa física a quem se referem os dados cadastrados;
 Encarregado: pessoa física que cuida dos dados;
@@ -35,33 +35,50 @@ Operador: pessoa que trata os dados em nome do controlador;
 Controlador: aquele que detém e faz decisões sobre o tratamento dos dados.
 </p>
 
-<h1>IV. Dos Tipos de Dados</h1>
-<p>
+<h1 style="text-align:justify">IV. Dos Tipos de Dados</h1>
+<p style="text-align:justify">
 No escopo da Mapa Cultural , os seguintes tipos de dados se aplicam:
 Dado anonimizado: todo dado que não permita a identificação do titular;
-Dado pessoal sensível: todo dado que permite a caracterização do titular;
+Dado pessoal sensível: dado pessoal sobre origem racial ou étnica, convicção 
+religiosa, opinião política, filiação a sindicato ou a organização de caráter 
+religioso, filosófico ou político, dado referente à saúde ou à vida sexual, 
+dado genético ou biométrico, quando vinculado a uma pessoa natural 
+(art. 5º, II  e art. 11 da LGPD);
 Dado pessoal: todo dado que permite a identificação do titular;
 </p>
 
-<h1>V. Da Classificação dos Dados</h1>
-<p>
+<h1 style="text-align:justify">V. Da Classificação dos Dados</h1>
+<p style="text-align:justify">
 No escopo da Mapa Cultural , as seguintes classificações dos dados se aplicam:
 Dados Privados: são os dados pessoais e dados sensíveis do titular;
 Dados Públicos: são os dados pessoais e/ou sensíveis, mas classificados como de acesso público pelo titular;
 Dados Anonimizados: são as informações consolidadas e derivadas dos dados pessoais e/ou sensíveis fornecidos pelo conjunto de titulares cadastrados na Mapa Cultural .
 </p>
 
-<h1>VI. Da Finalidade no Uso dos Dados</h1>
-<p>
+<h1 style="text-align:justify">VI. Da Finalidade no Uso dos Dados</h1>
+<p style="text-align:justify">
 No escopo da Mapa Cultural , dentre várias finalidades no uso dos dados, destacamos:
-Os dados privados fornecidas pelo titular e tratadas pelo Mapa Cultural  serão utilizadas para o fornecimento do serviço, respeitando as funcionalidades descritas neste documento;
-Geração de índices e indicadores relacionados aos registros dos espaços, agentes, eventos, projetos e oportunidades;
-Criação de mapas com georreferenciamento, podendo ser utilizados para pesquisas acadêmicas e/ou para a geração/aplicação/acompanhamento de políticas públicas de incentivo e desenvolvimento cultural;
-Estas informações poderão ser utilizadas pela Secretaria da Cultura do Estado  ou transferidas a outros Órgãos do Governo, sempre que houver o requerimento formal por parte destes, autorização do titular e, por último, quando a legislação o autorizar/demandar.
+    • Os dados privados fornecidos pelo titular e tratados pelo Mapa Cultural serão 
+    utilizados para o fornecimento do serviço, respeitando as funcionalidades descritas 
+    neste documento;
+    • Geração de índices e indicadores relacionados aos registros dos espaços, agentes, 
+    eventos, projetos e oportunidades;
+    • Criação de mapas com georreferenciamento, podendo ser utilizados para pesquisas 
+    acadêmicas e/ou para a geração/aplicação/acompanhamento de políticas públicas de 
+    incentivo e desenvolvimento cultural;
+    • Os dados pessoais sensíveis serão coletados e processados exclusivamente para os 
+    fins especificados no momento da obtenção do consentimento. 
+    • Os dados sensíveis não serão utilizados para finalidades diferentes daquelas para 
+    as quais o consentimento foi obtido, a menos que seja necessária uma nova obtenção 
+    de consentimento explícito do usuário.
+    • Estas informações poderão ser utilizadas pela Secretaria da Cultura do Estado ou 
+    transferidas a outros Órgãos do Governo, sempre que houver o requerimento formal por 
+    parte destes, autorização do titular e, por último, quando a legislação o 
+    Autorizar/demandar.
 </p>
 
-<h1>VII. Do Tratamento dos Dados</h1>
-<p>
+<h1 style="text-align:justify">VII. Do Tratamento dos Dados</h1>
+<p style="text-align:justify">
 O tratamento dos dados registrados na Mapa Cultural  observa a boa-fé e os seguintes princípios:
 I - finalidade: todos os dados coletados e armazenados na Mapa Cultural , serão utilizados no espírito exemplificado na seção “VI. Da Finalidade no Uso dos Dados na Mapa Cultural ”;
 II - adequação: os dados coletados e armazenados pela Mapa Cultural  só serão utilizados no atendimento às finalidades criadas pelas e nas consultas e/ou inscrições realizadas pelo titular;
@@ -73,61 +90,83 @@ VII - segurança: os dados armazenados na Mapa Cultural  utilizam-se de técnica
 VIII - prevenção: a adoção de boas práticas na verificação e na execução dos tratamentos dos dados armazenados na Mapa Cultural , previnem a ocorrência de danos aos titulares;
 IX - não discriminação: é garantido que nem os controladores, nem os operadores utilizarão os dados armazenados na Mapa Cultural  para fins discriminatórios ilícitos ou abusivos;
 X - responsabilização e prestação de contas: a plataforma é submetida, a testes de resistência de acesso e/ou uso de dados indevidos.
+XI - O titular expressamente consente, de forma livre, informada e einequívoca, para o tratamento de seus dados pessoais pelo Mapa Cultural, para as finalidades descritas neste Termo de Uso, conforme exigido pela LGPD
 </p>
 
-<h1>VIII. Da Guarda dos Dados</h1>
-<p>
+<h1 style="text-align:justify">VIII. Da Guarda dos Dados</h1>
+<p style="text-align:justify">
 As informações dos titulares registradas no Mapa Cultural  ficarão disponibilizadas por tempo indeterminado, podendo, o titular, a qualquer tempo, rever e apagar as suas informações.
 A eliminação de informações, por parte do titular, antes que todos os prazos legais que exijam a sua manutenção é de inteira responsabilidade deste, não podendo a Mapa Cultural  ser responsabilizada por esta ação.
+
+<b><i>A Secretaria da Cultura do Estado assume a responsabilidade pela proteção e segurança dos dados pessoais tratados pelo Mapa Cultural, conforme exigido pela LGPD.</i></b>
+
 </p>
 
-<h1>IX. Dos Deveres do Titular</h1>
-<p>
+<h1 style="text-align:justify">IX. Dos Deveres do Titular</h1>
+<p style="text-align:justify">
 A utilização do Mapa Cultural  somente pode se dar para propósitos lícitos e de modo a não afetar os direitos ou possibilidades de uso do sistema por terceiros. Não serão toleradas condutas ilícitas, imorais ou que possam molestar ou ser inconvenientes para os administradores do sistema, para os serviços disponibilizados ou para terceiros. Da mesma forma, o Mapa Cultural  não pode ser utilizado para difamar, abusar, assediar, perseguir, ameaçar ou violar quaisquer direitos individuais ou para a transmissão de conteúdo ilícito.
 O titular tem responsabilidade pessoal sobre suas informações de acesso ao Mapa Cultural  (login e senha) e deverá mantê-las em segurança, estritamente sob seu controle pessoal, não franqueando, em nenhuma circunstância, seu acesso a terceiros.
 O titular é responsável pela veracidade e pela atualização dos dados que informar ao Mapa Cultural , bem como aos serviços disponíveis através dele. O fornecimento de informação falsa poderá causar desde resultados inexatos e indesejados em relação aos serviços, bem como suspender ou cancelar o acesso ao sistema e ainda poderá punir o responsável por qualquer ato, conforme a legislação aplicável ao caso.
 O titular deverá comunicar imediatamente ao suporte do Mapa Cultural , através do email <a href="mailto:mapacultural@santaluzia.mg.gov.br">mapacultural@santaluzia.mg.gov.br</a> acerca de qualquer suspeita de acesso indevido ao sistema, de falha de segurança ou do comprometimento de suas informações de acesso.
 A não observância pelo titular, dos seus deveres previstos nestes Termos de Uso ou em qualquer outra legislação aplicável, poderá acarretar na suspensão ou no cancelamento do seu acesso ao Mapa Cultural , ou a um ou mais serviços disponibilizados, além das outras sanções previstas em Lei.
 </p>
-
-<h1>X. Direitos do Titular</h1>
-<p>
+<h1 style="text-align:justify">X. Direitos do Titular</h1>
+<p style="text-align:justify">
 O acesso às informações pessoais dos titulares, indicadas como privadas no seu perfil, não poderá ser comercializado e somente poderá ser efetuado por servidores autorizados (operadores) pela Secretaria da Cultura do Estado  ou por ordem judicial.
 São direitos reconhecidos pela Mapa Cultural  os definidos no artigo 18 da lei 13.709/2018.
+
+Os titulares dos dados pessoais têm direito de acesso, retificação, portabilidade, anonimização, bloqueio, eliminação e informação sobre o compartilhamento de seus dados, nos termos do artigo 18 da LGPD.
+
 </p>
 
-<h1>XI. Responsabilidades dos Administradores</h1>
-<p>
+<h1 style="text-align:justify">XI. Responsabilidades dos Administradores</h1>
+<p style="text-align:justify">
 A Secretaria da Cultura do Estado  é responsável pela segurança das informações coletadas, armazenadas e processadas pelo Mapa Cultural , bem como pela segurança das informações transmitidas aos serviços disponibilizados através do Mapa Cultural .
 A Secretaria da Cultura do Estado  utilizará as medidas adequadas, para maximizar a segurança e a integridade das informações pessoais sob sua custódia e evitar qualquer modalidade de acesso indevido.
 Com o intuito de assegurar a liberdade de expressão e impedir a censura, a Secretaria da Cultura do Estado  segue os princípios e garantias do Marco Civil da Internet, Lei nº 12.965, de 23 de abril de 2014.
+
+
+    • O Mapa Cultural implementará medidas técnicas e organizacionais adequadas para garantir a proteção de dados desde a concepção e por padrão, conforme exigido pela LGPD.
+    • Em caso de incidente de segurança de dados pessoais, o Mapa Cultural notificará as autoridades competentes e os titulares dos dados afetados, conforme exigido pela LGPD.
+    • No caso de transferência internacional de dados, o Mapa Cultural adotará medidas adequadas para garantir a proteção dos dados pessoais transferidos, conforme exigido pela LGPD.
+    • O Mapa Cultural nomeará um Encarregado de Proteção de Dados (Data Protection Officer - DPO), conforme exigido pelo artigo 41 da LGPD, que será responsável por garantir o cumprimento da legislação de proteção de dados.
+
 </p>
 
-<h1>XII. Política de Uso de Dados Pessoais</h1>
-<p>
+<h1 style="text-align:justify">XII. Política de Uso de Dados Pessoais</h1>
+<p style="text-align:justify">
 As informações pessoais fornecidas pelo titular e tratadas pelo Mapa Cultural  serão utilizadas para o fornecimento do serviço, com suas funcionalidades descritas nestes Termos de Uso.
 Estas informações poderão ser utilizadas pela Secretaria da Cultura do Estado  ou transferidas a outros Órgãos do Governo, sempre que houver o requerimento formal por parte destes, autorização do titular e, por último, quando a legislação o autorizar.
 </p>
 
-<h1>XIII. Condições gerais</h1>
-<p>
-Ao utilizar o Mapa Cultural , o titular será informado previamente sobre quais as informações pessoais serão de visualização pública e quais são privadas. 
-As informações do titular do Mapa Cultural  somente poderão ser modificadas através do sítio web do sistema, mediante o uso de suas informações de acesso.
-Caso o titular tenha perdido as suas informações de acesso, ele poderá utilizar o serviço de recuperação deste, disponibilizados no site do Mapa Cultural  ou através da equipe técnica no email <a href="mailto:suportemapacultural@santaluzia.mg.gov.br">suportemapacultural@santaluzia.mg.gov.br</a>.
+<h1 style="text-align:justify">XIII. Condições gerais</h1>
+<p style="text-align:justify">
+Ao utilizar o Mapa Cultural , o titular será informado previamente sobre quais as 
+informações pessoais serão de visualização pública e quais são privadas. 
+As informações do titular do Mapa Cultural  somente poderão ser modificadas através 
+do sítio web do sistema, mediante o uso de suas informações de acesso. Os dados pessoais 
+sensíveis serão coletados e processados exclusivamente para os fins especificados 
+no momento da obtenção do consentimento. Os dados sensíveis não serão utilizados para 
+finalidades diferentes daquelas para as quais o consentimento foi obtido, a menos que 
+seja necessária uma nova obtenção de consentimento explícito do usuário. 
+Caso o titular tenha perdido as suas informações de acesso, ele poderá utilizar o 
+serviço de recuperação deste, disponibilizados no site do Mapa Cultural  ou através 
+da equipe técnica no email <a href="mailto:suportemapacultural@santaluzia.mg.gov.br">
+suportemapacultural@santaluzia.mg.gov.br</a>.
 </p>
 
-<h1>XIV. Suspensão ou Término da Disponibilização</h1>
-<p>
+<h1 style="text-align:justify">XIV. Suspensão ou Término da Disponibilização</h1>
+<p style="text-align:justify">
 O Mapa Cultural  poderá ser suspenso ou ter sua disponibilização cessada a qualquer momento, mediante comunicação prévia, ou por e-mail, ou por postagem veiculada na página oficial da Secretaria da Cultura do Estado , aos seus titulares.
 </p>
 
-<h1>XV. Fundamentos Legais</h1>
-<p>
+<h1 style="text-align:justify">XV. Fundamentos Legais</h1>
+<p style="text-align:justify">
 Sem prejuízo de outras legislações, fundamentam o presente termo e a presente política – Constituição Federal – Tratados Internacionais – Lei 12.965/2014 (marco regulatório da internet no Brasil); Lei 9.610/1998 (Lei dos Direitos Autorais); Lei 17.449/2018 (lei do Sistema Estadual de Cultura); Lei 13.709/2018 (Lei Geral de Proteção de Dados); Lei 12.527/2011 (Lei de Acesso à Informação); Lei 8.159/1991 (Lei de Arquivos Públicos e Privados);
 </p>
 
-<h1>XVI. Alteração dos termos de uso</h1>
-<p>
+<h1 style="text-align:justify">XVI. Alteração dos termos de uso</h1>
+<p style="text-align:justify">
 As condições especificadas nestes Termos de Uso poderão ser modificadas a qualquer momento. Caso estas sejam significativas ou afetem, de forma concreta, os direitos e deveres dos titulares do sistema, serão apresentadas ao titular, de forma clara, para apreciação e concordância.
 Última modificação em 01/09/2020, às 10:30
 </p>
