efddb38960d582e447c8266b237a89e1db14d6d0
denysaraujo <denilson.araujo.info@gmail.com>
2024-07-05 14:07:34 -0300

    Ajuste nas informações do Mapa

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -2,21 +2,21 @@
 
 use MapasCulturais\i;
 
-date_default_timezone_set('America/São_Paulo');
+date_default_timezone_set('America/Sao_Paulo');
 
 $base_domain = @$_SERVER['HTTP_HOST'];
 $base_url = 'https://' . $base_domain . '/';
 
 return [
     'app.siteName' => env('SITE_NAME', 'Mapa Cultural'),
-    'app.siteDescription' => env('SITE_DESCRIPTION', "O Mapa Cultural é a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria da Cultura do Estado da Paraíba o sobre cenário cultural Paraíbano. Ficou mais fácil se programar para conhecer as opções culturais que as cidades cearenses oferecem: shows musicais, espetáculos teatrais, sessões de cinema, saraus, entre outras opções. Além de conferir a agenda de eventos, você também pode colaborar na gestão da cultura do estado: basta criar seu perfil de agente culturale e, e partir desse cadastro, facilitará a sua participação em editais e programas da Secretaria, além de divulgar seus eventos, espaços ou projetos."),
+    'app.siteDescription' => env('SITE_DESCRIPTION', "A plataforma livre, colaborativa e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas pela Secretaria Municipal de Cultura e Turismo de Santa Luzia-MG. O Mapa cultural é uma ferramenta de comunicação que visibiliza os eventos do circuito de festivais de artes e do calendário cultural, os projetos desenvolvidos e os espaços promovidos pelos agentes e instituições culturais de Santa Luzia. É também a plataforma de acesso e execução dos editais realizados pela Secretaria Municipal de Cultura e Turismo. Além de conferir a agenda de eventos, você também pode colaborar na gestão da cultura do estado: basta criar seu perfil de agente cultural. A partir do cadastro, fica mais fácil participar dos editais e programas da Secretaria e também divulgar seus eventos, espaços ou projetos"),
 
     'themes.active' => env('ACTIVE_THEME', 'SantaLuziaMG'),
 
     'app.lcode' => env('APP_LCODE', 'pt_BR'),
 
     'app.enabled.apps' => false,
-        
+
     ## Esse módulo é para configurar a funcionalidade de denúncia e/ou sugestões
     'google-recaptcha-sitekey' => env('GOOGLE_RECAPTCHA_SITEKEY', ''),
     'google-recaptcha-secret' => env('GOOGLE_RECAPTCHA_SECRET', ''),
