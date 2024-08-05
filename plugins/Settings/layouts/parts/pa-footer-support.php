686935a587ae8b57357c18b3be85b6f073bb9c92
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2024-08-05 00:54:06 -0300

    Refatoração dos Scripts, Códigos e Pastas da Base Project

--- plugins/Settings/layouts/parts/pa-footer-support.php
+++ plugins/Settings/layouts/parts/pa-footer-support.php
@@ -1,3 +1,3 @@
 <div class="pa-footer-support">
-    Precisa de ajuda? Envie um email para <a href="mailto:mapacultural@santaluzia.mg.gov.br">mapacultural@santaluzia.mg.gov.br</a>
+    Precisa de ajuda? Envie um email para <a href="mailto:mapacultural@santaluzia.mg.gov.br">mapacultural@santaluzia.mg.gov.br.</a>
 </div>
