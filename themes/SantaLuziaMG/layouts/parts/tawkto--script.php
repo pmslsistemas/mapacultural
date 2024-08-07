54d3d2549cc1ac5481ec16b84a6bca82fecee3b7
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2024-08-07 00:36:18 -0300

    Inclui o v-libras e o tawkto

--- themes/SantaLuziaMG/layouts/parts/tawkto--script.php
+++ themes/SantaLuziaMG/layouts/parts/tawkto--script.php
@@ -1,15 +1,13 @@
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/5f47c17bcc6a6a5947af53ba/default';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
-</script>
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/655c005191e5c13bb5b2063b/1hfnm2g4p';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
 <!--End of Tawk.to Script-->
